01fcff14b6355516fe974aca7567305230b15752
Hancyk Hanc <hancyk7@gmail.com>
2023-01-30 00:09:38 +0100

    Zadanie domowe dzień 4

--- FirstProjectApp/FirstProjectApp/Program.cs
+++ FirstProjectApp/FirstProjectApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Karolina";
+string sex = "kobieta";
+var age = 28;
+
+if (sex == "kobieta")
+
+    if (name == "Karolina" && age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    { 
+        Console.WriteLine("Ewa, lat 33");
+    }
+else
+    { 
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
