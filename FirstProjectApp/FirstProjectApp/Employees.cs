8702a834e5a866de679a191911cb3abf7603192f
Hancyk Hanc <hancyk7@gmail.com>
2023-01-31 23:23:10 +0100

    Zadanie z dnia 5

---
+++ FirstProjectApp/FirstProjectApp/Employees.cs
@@ -0,0 +1,22 @@
+﻿
+namespace FirstProjectApp
+{
+    public class Employees
+    {private List<int> scores = new List<int>();
+public Employees(string Name, string Surname, int Age ) 
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+        }
+        public string Name { get;private set; }
+        public string Surname { get;private set; }
+        public int Age { get;private set; }
+        public int Result
+        { get
+            { return this.scores.Sum(); }
+        }
+        public void AddScore(int newscores)
+        { this.scores.Add(newscores); } 
+    }
+}
--- FirstProjectApp/FirstProjectApp/Program.cs
+++ FirstProjectApp/FirstProjectApp/Program.cs
@@ -1,18 +1,40 @@
-﻿string name = "Karolina";
-string sex = "kobieta";
-var age = 28;
+﻿using FirstProjectApp;
 
-if (sex == "kobieta")
+Employees Employee1 = new Employees("Wojtek", "Nowak", 32);
+Employees Employee2 = new Employees("Ewa", "Kowalska", 28);
+Employees Employee3 = new Employees("Michał", "Zając", 35);
 
-    if (name == "Karolina" && age < 30)
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-    else
-    { 
-        Console.WriteLine("Ewa, lat 33");
-    }
-else
-    { 
-        Console.WriteLine("Niepełnoletni Mężczyzna");
-    }
+Employee1.AddScore(5);
+Employee1.AddScore(7);
+Employee1.AddScore(4);
+Employee1.AddScore(6);
+Employee1.AddScore(2);
+
+Employee2.AddScore(7);
+Employee2.AddScore(8);
+Employee2.AddScore(9);
+Employee2.AddScore(7);
+Employee2.AddScore(10);
+
+Employee3.AddScore(6);
+Employee3.AddScore(8);
+Employee3.AddScore(9);
+Employee3.AddScore(8);
+Employee3.AddScore(7);
+
+List<Employees> Employee = new List<Employees>()
+{
+    Employee1, Employee2, Employee3
+};
+int MaxResult = 0;
+Employees EmployeesWithMaxResults = null;
+
+foreach (var employees in Employee)
+{ if (employees.Result > MaxResult)
+    { MaxResult = employees.Result;
+        EmployeesWithMaxResults = employees;
+     }
+ }
+Console.WriteLine("Pracownik z największą ilościoą punktów (" + EmployeesWithMaxResults.Result + ") to:" + EmployeesWithMaxResults.Name +" "+ EmployeesWithMaxResults.Surname +" "+ EmployeesWithMaxResults.Age);
+Console.WriteLine("Gratulacje!");
+
